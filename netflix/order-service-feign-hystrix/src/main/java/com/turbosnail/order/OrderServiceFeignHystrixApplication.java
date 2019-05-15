6438bcf166c771e2a4355a77538e261420fff32f
zou <zouxq412@foxmail.com>
2019-05-15 10:11:20 +0800

    add Hystrix

--- netflix/order-service-feign-hystrix/src/main/java/com/turbosnail/order/OrderServiceFeignHystrixApplication.java
+++ netflix/order-service-feign-hystrix/src/main/java/com/turbosnail/order/OrderServiceFeignHystrixApplication.java
@@ -4,15 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-//@EnableCircuitBreaker
+@EnableCircuitBreaker
 public class OrderServiceFeignHystrixApplication {
 
     public static void main(String[] args) {
--- netflix/order-service-ribbon-hystrix/src/main/java/com/turbosnail/order/OrderServiceRibbonHystrixApplication.java
+++ netflix/order-service-ribbon-hystrix/src/main/java/com/turbosnail/order/OrderServiceRibbonHystrixApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
